5e7e142896395c0910ca01d653a6348a73b63571
charmines <47259933+ilikestohack@users.noreply.github.com>
2025-06-19 03:51:55 -0400

    Add shift to fully clear queue

--- plugins/ClearQueue/src/index.ts
+++ plugins/ClearQueue/src/index.ts
@@ -9,16 +9,32 @@ trace.msg.log(`Loaded!`);
 // Used to clean up resources, even listener dispose etc should be added here
 export const unloads = new Set<LunaUnload>();
 
+let shifting = false;
+
 // Log to console whenever changing page
 redux.intercept("playQueue/CLEAR_QUEUE", unloads, (payload) => {
+	if (shifting) return false;
+
 	const playQueue = redux.store.getState().playQueue;
 	const playQueueElementsCopy = [...playQueue.elements];
 	const removedItems = playQueueElementsCopy.splice(playQueue.currentIndex + 1);
 
-	console.log(payload, playQueue, playQueueElementsCopy, removedItems);
+	// console.log(payload, playQueue, playQueueElementsCopy, removedItems);
 
 	for (const song of removedItems) {
-		console.log(song, redux.actions["playQueue/REMOVE_ELEMENT"]({ uid: song.uid }));
+		redux.actions["playQueue/REMOVE_ELEMENT"]({ uid: song.uid });
 	}
 	return true;
+});
+
+function setShift(state = true) {
+	shifting = state;
+}
+
+window.addEventListener("keydown", () => setShift());
+window.addEventListener("keyup", () => setShift(false));
+
+unloads.add(() => {
+	window.removeEventListener("keydown", () => setShift());
+	window.removeEventListener("keyup", () => setShift(false));
 });
